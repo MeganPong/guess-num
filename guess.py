7884a85b3d8e0271840ac87878bd7b8205df6045
Megan <meganpong88@yahoo.com.tw>
2022-03-22 14:32:04 +0800

    final version

--- guess.py
+++ guess.py
@@ -4,19 +4,27 @@
 # 猜錯的話 要告訴他 比答案大/小
 
 import random
-
-r = random.randint(1, 100)
+count = 0
+start = input('請決定範圍開始值: ')
+end = input('請決定範圍結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 while True:
+	count += 1 #count = count + 1
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
 		print('終於答對了!')
+		print('這是你猜的', count, '次')
 		break
 	elif num > r:
 		print('比答案大')
+		print('這是你猜的', count, '次')
 	elif num < r:
 		print('比答案小')
-		
+		print('這是你猜的', count, '次')
+
 
     
 
